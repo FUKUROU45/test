01d7c341b23fb1a75368ca45decba2b27c33fedd
FUKUROU45 <jbk3150iliab@gmail.com>
2025-05-22 07:49:11 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
-import streamlit as it
+import streamlit as st
 
 st.title('あいうえお')
 
 st.write('aaaaaaaaa')
+
+user_name = st.text_imput('名前を入力してください')
+
+st.header('あなたの名前は' + str(user_name) + 'です')
+h = st.text_imput
