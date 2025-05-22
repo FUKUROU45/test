5c326633addce5eececc1c34cdb1e3df6a6fe7af
FUKUROU45 <jbk3150iliab@gmail.com>
2025-05-22 08:03:10 +0000

    aaaaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ user_name = st.text_input('名前を入力してください')
 
 st.header('あなたの名前は' + str(user_name) + 'です')
 
-h = st.text_input('身長を入力してください(m',value=1.67)
+h = st.number_input('身長を入力してください(m',value=1.67)
 w = st.number_input('体重を入力してください(kg)',value=60)
 
 bmi = w/h(h**2)
