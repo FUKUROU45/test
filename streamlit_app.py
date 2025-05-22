80c4172a70d90bee754bc4228db38a911e1db9c2
FUKUROU45 <jbk3150iliab@gmail.com>
2025-05-22 08:04:05 +0000

    カービー

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.header('あなたの名前は' + str(user_name) + 'です')
 h = st.number_input('身長を入力してください(m',value=1.67)
 w = st.number_input('体重を入力してください(kg)',value=60)
 
-bmi = w/h(h**2)
+bmi = w/(h**2)
 
 dbmi = round(bmi,2)
 
