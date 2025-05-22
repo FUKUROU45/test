68c1099607c08b913d7963729500c0699c0636f2
FUKUROU45 <jbk3150iliab@gmail.com>
2025-05-22 07:59:06 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
-st.title('あいうえお')
+st.title('自己紹介')
 
-st.write('aaaaaaaaa')
+st.write('あなたの名前は')
 
 user_name = st.text_imput('名前を入力してください')
 
 st.header('あなたの名前は' + str(user_name) + 'です')
-h = st.text_imput
+
+h = st.text_imput('身長を入力してください(m',value=1.67)
+w = st.number_input('体重を入力してください(kg)',value=60)
+
+bmi = w/h(h**2)
+
+dbmi = round(bmi,2)
+
+st.header('あなたのBMIは'+str(dbmi)+'です')
