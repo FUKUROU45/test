73114ca7aa0e53be32f75625a6df67b6c125cc64
FUKUROU45 <jbk3150iliab@gmail.com>
2025-06-10 08:42:36 +0000

    wa

---
+++ vocab_data.py
@@ -0,0 +1,8 @@
+# vocab_data.py などに保存可
+word_list = [
+    {"word": "apple", "meaning": "りんご"},
+    {"word": "book", "meaning": "本"},
+    {"word": "car", "meaning": "車"},
+    {"word": "dog", "meaning": "犬"},
+    {"word": "elephant", "meaning": "象"},
+]
